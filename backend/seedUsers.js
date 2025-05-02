23a16502974eead8ab97b2de9e6c5c694e1eec9d
Shaik Milad <shaikmeeladh@gmail.com>
2025-05-02 22:12:18 +0530

    Update seedUsers.js

--- backend/seedUsers.js
+++ backend/seedUsers.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('./models/User');
 
 const MONGODB_URI = process.env.MONGODB_URI;
